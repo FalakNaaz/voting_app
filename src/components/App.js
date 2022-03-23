911f901140f6cf1393321ff6ac1c9914025c9b2b
Falak Naz <nazfalaknaz@gmail.com>
2022-03-23 17:37:20 +0530

    login

--- src/components/App.js
+++ src/components/App.js
@@ -1,135 +1,54 @@
-// import React, {Component} from 'react'
+import React, { useCallback }  from 'react';
+//import logo from './logo.png';
+//import './App.css';
+import { useMoralis } from "react-moralis";
+import Home from "./Home.js";
+import {useHistory, Link, Route, useNavigate} from 'react-router-dom';
 
-// class App extends Component{
-//     render(){
-//         return(
-//             <div>
-//                 <h1> Hello World</h1>
-//             </div>
-//         )
-//     }
-// }
 
-// export default App;
-
-import React, {Component} from 'react'
-import Navbar from './Navbar'
-import Web3 from 'web3'
-import Ballot from '../truffle_abis/Ballot.json'
-import Main from './Main.js'
-
-class App extends Component{
-    
-    async componentWillMount(){
-        await this.loadWeb3()
-        await this.loadBlockchainData()
-    }
-    async loadWeb3(){
-        if(window.ethereum){
-            window.web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-        }else if(window.web3){
-            window.web3 = new Web3(window.web3.currentProvider)
-        }else{
-            alert('No ethereum browser found! you can check metamask!')
-        }
-    }
-    async loadBlockchainData(){
-        const ethers =  require('ethers');
-        const web3 = window.web3
-        const account = await web3.eth.getAccounts()
-        this.setState({account: account[0]})
-        //console.log(account,'hey hey')
-        const networkId =await  web3.eth.net.getId()
-        //console.log(networkId,'Network ID')
-
-        //load tether data
-        const ballotData = Ballot.networks[networkId]
-        let ballot
-        if(ballotData){
-            let temp = []
-             ballot = new web3.eth.Contract(Ballot.abi, ballotData.address)
-            this.setState({ballot:ballot})
-
-            let l = await this.state.ballot.methods.cLength().call();
-            for(let i = 0; i < l; i++){
-                let namee = await this.state.ballot.methods.candidates(i).call();
-                namee = ethers.utils.parseBytes32String(namee.name)
-                //console.log(namee.name)
-                temp[i] = namee
-                
-            }
-            this.setState({candidateNames: temp})
-        }else{
-            window.alert('Error! tether contract not deployed - no detected network!')
-        }
-
-        this.setState({loading:false})
-
-    }
-      voteFunction = (candidate) => {
-        this.setState({loading: true })
-        this.state.ballot.methods.vote(candidate).send({from: this.state.account}).on('transactionHash', (hash) => {
-          this.setState({loading:false})
-        }) 
-      }
-       winningCandidateName =   async() => {
-        const ethers =  require('ethers');
-        let res = await this.state.ballot.methods.winningCandidateName().call();
-        res = ethers.utils.parseBytes32String(res)
-        console.log("The winner is: ", res)
-        this.setState({res: res.toString() }) 
-      }
-
+function App() {
+  
+  //const handleOnClick = () => history.push('./Home.js');
+  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+  const navigate = useNavigate();
+    const login = async () => {
       
-    
-    constructor(props){
-        super(props)
-        this.state = {
-            account: '0x0',
-            ballot: {},
-            res: '0',
-            loading: true,
-            candidateNames: []
-            
-            
-        }
-    }
-    render(){
-        let content
-        {this.state.loading ?
-             content= <p id='loader' className='text-center' style={{margin: '30px'}}>Loading...</p> :
-              content=
-               <Main 
-               res = {this.state.res}
-                voteFunction = {this.voteFunction}
-                winningCandidateName= {this.winningCandidateName}
-                candidateNames = {this.state.candidateNames}
-                //ballot = {this.state.ballot}
-               />
-            }
-        return(
-            
+      if (!isAuthenticated) {
 
-            <div >
-                
-                     <Navbar account = {this.state.account}/>
-                   <div className='container-fluid mt-5'>
-                        <div className='row'>
-                            <main role='main' className='col-lg-12 ml-auto mr-auto' style={{maxWidth : '600px', minHeight : '100vm'}}>
-                                {content}
-                            </main>
-
-                        </div>
-                        
-                    </div> 
-                    
-                    
-                    
-            </div>
+        await authenticate({signingMessage: "Log in using Moralis" })
+          .then(function (user) {
             
-        )
+            console.log("logged in user:", user);
+            console.log(user.get("ethAddress"));
+            //this.props.history.push("/home");
+            navigate("/home", { replace: true });
+
+            // <Route render={({ history}) => (
+            //   history.push('/home')
+            // )} />
+          
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
     }
+
+    const logOut = async () => {
+      await logout();
+      console.log("logged out");
+    }
+
+  return (
+    <div>
+      
+      <h1>Moralis Hello World!</h1>
+      <button onClick={login}>Moralis Metamask Login</button>
+      {/* <Link to="./Home.js" className="btn btn-primary">hello</Link> */}
+      <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+      
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+import {useNavigate, Link, Router, Routes,Route, BrowserRouter} from 'react-router-dom';
+//import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
+// ReactDOM.render(
+//     <React.StrictMode>
+//     <MoralisProvider serverUrl="https://obtz1utqtwxn.usemoralis.com:2053/server" appId="BU9h9ioUi5crW9o8GDCqwHlAQTKA2lR7LCBTZKEj">
+//       <App />
+//     </MoralisProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    
+  <React.StrictMode>
+   <MoralisProvider serverUrl="https://obtz1utqtwxn.usemoralis.com:2053/server" appId="BU9h9ioUi5crW9o8GDCqwHlAQTKA2lR7LCBTZKEj">
+       <BrowserRouter>
+       <App />
+       <Routes>
+        <Route path="/home" element=  {<Home />}
+ ></Route>
+        <Route path="/app" component= {App}></Route>
+       </Routes>
+       </BrowserRouter>
+     </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
