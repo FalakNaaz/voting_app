7188ba7bdb96e42e7d0c4004cbcfe8fb1e11452c
Falak Naz <nazfalaknaz@gmail.com>
2022-04-01 15:07:13 +0530

    fixed-logout

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React, { useCallback }  from 'react';
 //import logo from './logo.png';
 import '../static/css/App.css';
 import { useMoralis } from "react-moralis";
+import {Moralis} from "moralis";
 import Register from "./Register.js";
 import {useHistory, Link, Route, useNavigate} from 'react-router-dom';
 import Logo from '../static/images/bbg.png';
 import Cir from '../static/images/cir.png';
 import Nav from "./Nav";
-
+const serverUrl = "https://obtz1utqtwxn.usemoralis.com:2053/server";
+const appId = "BU9h9ioUi5crW9o8GDCqwHlAQTKA2lR7LCBTZKEj";
+Moralis.start({ serverUrl, appId });
 
 function App() {
   
@@ -16,36 +19,23 @@ function App() {
   const navigate = useNavigate();
   
     const login = async () => {
-      console.log(isAuthenticated)
-      if (!isAuthenticated) {
-
-        await authenticate({signingMessage: "Log in using Moralis" })
-          .then(function (user) {
-            
-            console.log("logged in user:", user);
-            console.log(user.get("ethAddress"));
-            //this.props.history.push("/home");
-            navigate("/home", { replace: true });
-
-            // <Route render={({ history}) => (
-            //   history.push('/home')
-            // )} />
-          
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+      let user = Moralis.User.current();
+        console.log(user)
+        if (!user) {
+          user = await Moralis.authenticate();
+          navigate("/home", { replace: true });
+        }
+        console.log("logged in user:", user);
     }
     const register = ()=>{
       navigate("/register", { replace: true });
 
     }
 
-    const logOut = async () => {
-      await logout();
-      console.log("logged out");
-    }
+    // const logOut = async () => {
+    //   await logout();
+    //   console.log("logged out");
+    // }
 
   return (
     <section className='wrapper'>
@@ -69,8 +59,8 @@ function App() {
       </div>
     </div>
       
-      <button className="btn btn-primary">hello</button> 
-       <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+      {/* <button className="btn btn-primary">hello</button>  */}
+       {/* <button onClick={logOut} >Logout</button> */}
     </section>
   );
 }
